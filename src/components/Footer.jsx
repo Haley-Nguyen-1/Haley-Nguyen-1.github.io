dd03fce392492e6b3193a25171bd99493a604e75
tony-ngo-03 <tonyngo@utexas.edu>
2024-08-14 12:52:36 -0500

    started css on header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import './Components.css';
 function Footer (){
-
     return (
-        <>
-            <h1>Footer</h1>
-            {/*Social Media, Mini Links, Contact Info, copyright??*/}
-        </>
+        <footer>
+            <h1 className="center-text">Elite Demolition Company</h1>
+            <menu>
+                <a href="/">Home</a>
+                <a href="/about-us">About Us</a>
+                <a href="/services">Services</a>
+                <a href="/contact">Contact</a>
+                <a href="/reviews">Reviews/Testimonials</a>
+            </menu>
+        </footer>
     );
 }
 
